2977198cd8cb6ddd4ce2673ef3dde2a10be2c436
Jan Kudlicka <github@kudlicka.eu>
2026-03-16 17:14:18 +0100

    Added all models to the admin.

--- issues/admin.py
+++ issues/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Issue, Comment, IssueType, Resolution
+
+admin.site.register(Issue)
+admin.site.register(Comment)
+admin.site.register(IssueType)
+admin.site.register(Resolution)
+
+
+
